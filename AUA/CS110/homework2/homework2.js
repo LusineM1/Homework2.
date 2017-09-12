a0a5f0e2aedd68275469e332ca97eff6151ead85
LusineM1 <15lusine@inbox.ru>
2017-09-12 18:36:18 +0400

    my first commit

---
+++ AUA/CS110/homework2/homework2.js
@@ -0,0 +1,21 @@
+const number1=3
+const number2='abc'
+const number3=18
+const sum = number2+number3; 
+console.log(number1*number2)
+console.log(sum)
+console.log('number1+number3')
+
+
+const number5=35;
+const number6=10
+if(number1+number2>=60){
+	console.log('You have won the game');
+}
+else{
+	console.log('No way, go home');
+}
+
+
+const sum1=number1+number3
+console.log (!(number5===sum1))
